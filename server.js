a721b035338b700ff69e86e0aebade0c2bbe736e
fuyanik <furkan.uyanik@boun.edu.tr>
2022-10-14 02:46:38 +0300

    asd

--- server.js
+++ server.js
@@ -30,6 +30,17 @@ app.post("/create-payment-intent", async (req, res) => {
   });
 });
 
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 4242;
 console.log("asfd");
 
