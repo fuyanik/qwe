cc8c1bde1e956c63babb5c1e7f42645167ca4d62
fuyanik <furkan.uyanik@boun.edu.tr>
2022-10-14 03:06:49 +0300

    asd

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 5000;
+
 // This is your test secret API key.
 const stripe = require("stripe")('sk_test_51LOrQYKGr3XuaAt4d9fT0XXB8CrL4GKbBHG3t8alb1Yo1aIULBvpbUm8UwDZNWNx5YRSatAzkHJH3Jlx25qlPtow00dTA8p2Ij');
 
@@ -41,7 +43,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 4242;
-console.log("asfd");
 
-app.listen(process.env.PORT || 4242, () => console.log(`Node server listening on port ${port}!`));
+
+
+app.listen(port, () => console.log(`Node server listening on port ${port}!`));
